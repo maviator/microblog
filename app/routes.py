aa23d1698557ca7863c34da7c46ca837838b7b8c
Mohtadi Ben Fraj <mohtedibf@gmail.com>
2019-05-18 15:36:50 +0200

    follow and unfollow users and some unit tests

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,38 @@ from werkzeug.urls import url_parse
 from datetime import datetime
 
 
+@app.route('/follow/<username>')
+@login_required
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You cannot follow yourself!')
+        return redirect(url_for('user', username=username))
+    current_user.follow(user)
+    db.session.commit()
+    flash('You are following {}!'.format(username))
+    return redirect(url_for('user', username=username))
+
+
+@app.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You cannot unfollow yourself!')
+        return redirect(url_for('user', username=username))
+    current_user.unfollow(user)
+    db.session.commit()
+    flash('You unfollowed {}.'.format(username))
+    return redirect(url_for('user', username=username))
+
+
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
